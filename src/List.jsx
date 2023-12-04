d83b55a58d5a07a450827919420bd5eaa94fcd10
Mucahit <mucahit.kartal2015@gmail.com>
2023-12-04 13:01:02 +0300

    final commit

---
+++ src/List.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const List = ({ people }) => {
+  return (
+    <div className="card">
+      {people.map(({ id, name, age, image }) => (
+        <div className="person" key={id}>
+          <img src={image} alt={name} />
+          <div>
+            <h4>{name}</h4>
+            <p>{age}</p>
+          </div>
+        </div>
+      ))}
+      {/* <button>Clear All</button> */}
+    </div>
+  );
+};
+
+export default List;
